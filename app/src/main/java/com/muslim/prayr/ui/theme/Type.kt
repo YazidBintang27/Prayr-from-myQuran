323cac784e456ea50417808e2116c4bb3e3a9d66
Yazid Bintang <yazidbintang2711@gmail.com>
2025-03-17 20:11:12 +0700

    Add theme

--- app/src/main/java/com/muslim/prayr/ui/theme/Type.kt
+++ app/src/main/java/com/muslim/prayr/ui/theme/Type.kt
@@ -2,33 +2,40 @@ package com.muslim.prayr.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.muslim.prayr.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
    bodyLarge = TextStyle(
-      fontFamily = FontFamily.Default,
-      fontWeight = FontWeight.Normal,
+      fontFamily = FontFamily(Font(R.font.raleway_bold)),
+      fontWeight = FontWeight.Bold,
       fontSize = 16.sp,
       lineHeight = 24.sp,
       letterSpacing = 0.5.sp
-   )
-   /* Other default text styles to override
+   ),
+   bodySmall = TextStyle(
+      fontFamily = FontFamily(Font(R.font.raleway_medium)),
+      fontWeight = FontWeight.Medium,
+      fontSize = 14.sp,
+      lineHeight = 24.sp,
+      letterSpacing = 0.5.sp
+   ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontFamily = FontFamily(Font(R.font.raleway_bold)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.raleway_medium)),
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
